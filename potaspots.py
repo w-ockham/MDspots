9cd64211a9901aa87cadbf399fb67e0048204726
w-ockham <minoru.tomobe@gmail.com>
2022-02-19 23:53:05 +0900

    Add activation log summary.

--- potaspots.py
+++ potaspots.py
@@ -6,6 +6,7 @@ import pickle
 import urllib.request
 import json
 import re
+import schedule
 import sys
 import sqlite3
 import time
@@ -18,9 +19,11 @@ access_sec = ''
 myid =  ''
 
 class POTASpotter:
-    def __init__(self, myid, interval=70, prefix = 'JA-*'):
+    def __init__(self, myid, interval=70, tweetat="21:00", logwindow=16, prefix = 'JA-*'):
         self.myid = myid;
         self.interval = interval
+        self.tweetat = tweetat
+        self.logwindow = logwindow
         self.prefix = prefix
         self.potaapi = 'https://api.pota.app/spot/activator/'
         self.logdir = '/var/log/potaspots/'
@@ -54,6 +57,7 @@ class POTASpotter:
     def logsearch(self, target, twindow):
         lastseen = self.now - twindow
         mesg = ''
+        count = 0
         
         q = f"select distinct callsign, ref from potaspots where utc > {lastseen}"
         if target:
@@ -74,7 +78,8 @@ class POTASpotter:
                 for cm in re.split('[, ;]', comment):
                     m = re.match('(\w+-\d\d\d\d)', cm)
                     if m :
-                        nfer.append(m.group(1))
+                        if not m.group(1) in nfer:
+                            nfer.append(m.group(1))
 
                     m = re.match('(\w+/\w+-\d+)', cm)
                     if m :
@@ -108,15 +113,17 @@ class POTASpotter:
                 refs += ' SOTA:' + sota
 
             mesg += f"{tm} {call} {ref}{refs} {fr}\n"
-
-        if mesg == '':
+            count += 1
+            
+        if count == 0:
             mesg = 'No Logs.'
-
-        return mesg
+    
+        return (count, mesg)
     
     def spotsearch(self, target, maxfreq, twindow):
-
         lastseen = self.now - twindow
+        mesg = ''
+        count = 0
 
         if maxfreq:
             q = f"select distinct callsign,ref  from potaspots where utc > {lastseen} and freq <= {maxfreq}"
@@ -126,7 +133,6 @@ class POTASpotter:
         if target:
             q += f" and region = '{target}'"
             
-        mesg = ''
         for s in self.cur.execute(q + ' order by utc desc'):
             (call, ref ) = s
             q = f"select * from potaspots where callsign ='{call}' and ref='{ref}' and utc > {lastseen} order by utc desc"
@@ -134,11 +140,12 @@ class POTASpotter:
             e = l.fetchone()
             (_, tm, call, freq, mode, region, ref, park, comment) = e
             mesg += f"{tm} {ref} {call} {freq} {mode} {comment}\n"
-
-        if mesg == '':
+            count += 1
+            
+        if count == 0:
             mesg = 'No Spots.'
-
-        return mesg
+            
+        return (count, mesg)
     
     def interp(self, cmd):
         self.now = int(datetime.datetime.utcnow().strftime("%s"))
@@ -153,16 +160,16 @@ class POTASpotter:
                 maxfreq = 100000
             elif 'LOG' in cmd:
                 logmode = True
-                twindow = 12 * 3600 #12H
+                twindow = 12 * 3600
             elif cmd.isdigit():
-                twindow = int(cmd) * 60 
+                twindow = int(cmd) * 60
             else:
                 break
 
         if logmode:
-            mesg = self.logsearch(region, twindow)
+            (_, mesg) = self.logsearch(region, twindow)
         else:
-            mesg = self.spotsearch(region, maxfreq, twindow)
+            (_, mesg) = self.spotsearch(region, maxfreq, twindow)
 
         return mesg
 
@@ -183,85 +190,117 @@ class POTASpotter:
             rcpt = usr["message_create"]["sender_id"]
             if rcpt != self.myid:
                 cmd = usr["message_create"]["message_data"]["text"]
+                mesg = self.interp(cmd)
                 msgevent = {
                     "type": "message_create",
                     "message_create": { "target":{"recipient_id": rcpt },
                                         "message_data":
-                                        {"text": self.interp(cmd)}}}
+                                        {"text": mesg }}}
                 try:
                     self.api.send_direct_message(event = msgevent)
                     self.log(msgevent)
                 except TwythonError as e:
                     self.log(f'Warning: {e.error_code}')
                     return
+            
+    def summary(self):
+        (count, mesg) = self.logsearch('JA', self.logwindow * 3600)
+        mesg = f"Today's activation summary: {count} parks\n" +mesg
+        if count > 0:            
+            tm = ''
+            for m in mesg.splitlines():
+                if len(tm + m) > 270:
+                    try:
+                        self.api.update_status(status=tm.rstrip())
+                        self.log(f'Spotted: {tm.rstrip()}')
+                    except TwythonError as e:
+                        self.log(f'Error: {e}')
+                    tm = m + '\n'
+                else:
+                    tm += m + '\n'
+            mesg = tm
+        try:
+            self.api.update_status(status=mesg.rstrip())
+            self.log(f'Spotted: {mesg.rstrip()}')
+        except TwythonError as e:
+            self.log(f'Error: {e}')
+
+    def periodical(self):
+        self.now = int(datetime.datetime.utcnow().strftime("%s"))
+        
+        try:
+            spotobj = urllib.request.urlopen(self.potaapi)
+            spotdata = json.loads(spotobj.read())
+        except Exception as e:
+            self.log(f'Error: {e}')
+            time.sleep(self.interval)
+            return
+
+        if spotdata:
+            spots =[s for s in spotdata[::-1]
+                    if int(s['spotId']) > self.lastid]
+            for s in spots:
+                sid = s['spotId']
+                ref = s['reference']
+                activator = s['activator']
+                freq = s['frequency']
+                mode = s['mode']
+                park = s['name']
+                loc  = s['locationDesc']
+                spotter = s['spotter']
+                comment = s['comments']
+                lat = s['latitude']
+                lon = s['longitude']
+                hhmm= datetime.datetime.fromisoformat(s['spotTime']).strftime('%H:%M')
+                mesg = f'{hhmm} {activator} on {ref}({loc} {park}) {freq} {mode} {comment}[{spotter}]'
+
+                m = re.match(self.prefix, ref)
+                if m:
+                    try:
+                        self.api.update_status(status=mesg, lat=lat, long=lon)
+                        self.log(f'Spotted id{sid}: {mesg}')
+                    except TwythonError as e:
+                        self.log(f'Error: {e}')
+                            
+                region = ref[0:ref.find('-')]                              
+                try:
+                    rfreq = float(freq)
+                except ValueError:
+                    rfreq = 0.0
+                q = 'insert into potaspots(utc, time,callsign,freq,mode,region, ref, park, comment) values(?, ?, ?, ?, ?, ?, ?, ?, ?)'
+                self.cur.execute(q, (self.now, hhmm, activator, rfreq, mode, region, ref, park, comment))
+                        
+                tlwindow = self.now - 3600 * 24 * 7
+                self.cur.execute(f'delete from potaspots where utc < {tlwindow}')
+
+            if spots:
+                self.lastid = max(int(i['spotId']) for i in spots)
+                self.log(f'Latest spot id{self.lastid}.')
+            else:
+                self.log(f'No spots since id{self.lastid}.')
+                
+            self.db.commit()
+                
+        else:
+            self.log(f'No Spots.')
+
+        self.check_dm()
+
+        with open(self.homedir + 'lastid.pkl', mode='wb') as f:
+            pickle.dump({'lastid':self.lastid, 'lastmsg':self.lastmsg}, f)
 
     def run(self):
+        schedule.every(self.interval).seconds.do(self.periodical)
+        schedule.every().day.at(self.tweetat).do(self.summary)
+
         while True:
-            self.now = int(datetime.datetime.utcnow().strftime("%s"))
-
-            try:
-                spotobj = urllib.request.urlopen(self.potaapi)
-                spotdata = json.loads(spotobj.read())
-            except Exception as e:
-                self.log(f'Error: {e}')
-                time.sleep(self.interval)
-                continue
-
-            if spotdata:
-                spots =[s for s in spotdata[::-1]
-                        if int(s['spotId']) > self.lastid]
-                for s in spots:
-                    sid = s['spotId']
-                    ref = s['reference']
-                    activator = s['activator']
-                    freq = s['frequency']
-                    mode = s['mode']
-                    park = s['name']
-                    loc  = s['locationDesc']
-                    spotter = s['spotter']
-                    comment = s['comments']
-                    lat = s['latitude']
-                    lon = s['longitude']
-                    hhmm= datetime.datetime.fromisoformat(s['spotTime']).strftime('%H:%M')
-                    mesg = f'{hhmm} {activator} on {ref}({loc} {park}) {freq} {mode} {comment}[{spotter}]'
-
-                    m = re.match(self.prefix, ref)
-                    if m:
-                        try:
-                            self.api.update_status(status=mesg, lat=lat, long=lon)
-                            self.log(f'Spotted id{sid}: {mesg}')
-                        except TwythonError as e:
-                            self.log(f'Error: {e}')
-                            
-                    region = ref[0:ref.find('-')]                              
-                    try:
-                        rfreq = float(freq)
-                    except ValueError:
-                        rfreq = 0.0
-                    q = 'insert into potaspots(utc, time,callsign,freq,mode,region, ref, park, comment) values(?, ?, ?, ?, ?, ?, ?, ?, ?)'
-                    self.cur.execute(q, (self.now, hhmm, activator, rfreq, mode, region, ref, park, comment))
-                    
-                    tlwindow = self.now - 3600 * 24 * 7
-                    self.cur.execute(f'delete from potaspots where utc < {tlwindow}')
-
-                if spots:
-                    self.lastid = max(int(i['spotId']) for i in spots)
-                    self.log(f'Latest spot id{self.lastid}.')
-                else:
-                    self.log(f'No spots since id{self.lastid}.')
-
-                self.db.commit()
-                
-            else:
-                self.log(f'No Spots.')
-
-            self.check_dm()
-
-            with open(self.homedir + 'lastid.pkl', mode='wb') as f:
-                pickle.dump({'lastid':self.lastid, 'lastmsg':self.lastmsg}, f)
-
-            time.sleep(self.interval)
+            schedule.run_pending()
+            time.sleep(10)
+        
 
 if __name__ == "__main__":
-  spotter = POTASpotter(myid, 70, 'JA-*')
+  spotter = POTASpotter(myid = myid,
+                        interval=70,
+                        tweetat="21:00",
+                        prefix='JA-*')
   spotter.run()
